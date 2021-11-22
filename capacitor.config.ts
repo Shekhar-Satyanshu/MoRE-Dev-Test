9ace8362a2641c7297752908e1971661f72e6ef6
Satyanshu Shekhar <satyanshushekhar@virtualemployee.com>
2021-11-22 13:17:45 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MoRE App Test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
